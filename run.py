d186dce12b49754eef8351e3970d38780c090b8c
Michael Sturgeon <1993msturgeon@gmail.com>
2024-06-10 08:28:24 +0000

    Create render_board function to print the game board and call the function in main function

--- run.py
+++ run.py
@@ -1,3 +1,22 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Board cell values
+cells = ["", " ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def render_board():
+    """
+    Display the game board to the user.
+    """
+    print(f"               {cells[1]} | {cells[2]} | {cells[3]}")
+    print(f"              ---|---|---")
+    print(f"               {cells[4]} | {cells[5]} | {cells[6]}")
+    print(f"              ---|---|---")
+    print(f"               {cells[7]} | {cells[8]} | {cells[9]}\n")
+
+
+def main():
+    """
+    Run the game functions.
+    """
+    render_board()          
+    
+
+main()
