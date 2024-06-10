30c6b551460c9b097e009160cf7c097bc7e7176e
Michael Sturgeon <1993msturgeon@gmail.com>
2024-06-10 08:37:12 +0000

    Create update_board function to get and validate user input before updating the game board

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 # Board cell values
 cells = ["", " ", " ", " ", " ", " ", " ", " ", " ", " "]
 
+# Current player variable
+current_player = "X"
+
+
 def render_board():
     """
     Display the game board to the user.
@@ -12,11 +16,33 @@ def render_board():
     print(f"               {cells[7]} | {cells[8]} | {cells[9]}\n")
 
 
+def update_board():
+    """
+    Update the game board with a valid input.
+    """
+    try:
+        choice = int(input("    Please enter an empty cell number: "))
+        if cells[choice] == " ":
+            cells[choice] = current_player                                                
+        else:
+            print("  ## This cell is taken, please choose and EMPTY cell! ##")
+        if choice < 1:
+            raise IndexError(" ## Please enter a number between 1-9! ##")
+    except IndexError:
+        print("  ## Please only enter a number between 1-9! ##")
+    except ValueError:
+        print("  ## Please enter a NUMBER between 1-9! ##")
+    else:
+        pass
+
+
 def main():
     """
     Run the game functions.
     """
-    render_board()          
+    while True:
+        render_board()
+        update_board()          
     
 
 main()
