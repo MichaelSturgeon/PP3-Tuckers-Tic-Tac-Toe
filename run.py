06d2c4fdbdc29c9e7b093e617a4c018b9e68b5ca
Michael Sturgeon <1993msturgeon@gmail.com>
2024-06-10 09:02:35 +0000

    Create game_instructions & render_legend functions and run them in main function

--- run.py
+++ run.py
@@ -4,6 +4,26 @@ cells = ["", " ", " ", " ", " ", " ", " ", " ", " ", " "]
 # Current player variable
 current_player = "X"
 
+def game_instructions():
+    """
+    Display the game instructions to the user.
+    """
+    print("        TIC-TAC-TOE INSTRUCTIONS\n")
+    print("    1: Take turns marking an empty cell!")
+    print("    2: Get 3 marks in a row to win!(up/down, across, or diagonal)")
+    print("    3: A legend is provided to help you make your moves!")
+    print("    4: When all the cells are full the game is over!")
+    print("    5: If no player has 3 in a row, the game ends in a tie!\n")
+
+
+def render_legend():
+    """
+    Display a legend showing the cell reference numbers.
+    """
+    print("    1|2|3")
+    print("    4|5|6")
+    print("    7|8|9")
+
 
 def render_board():
     """
@@ -40,7 +60,9 @@ def main():
     """
     Run the game functions.
     """
+    game_instructions()
     while True:
+        render_legend()
         render_board()
         update_board()          
     
